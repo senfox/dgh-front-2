b2b50305a2dec65a89169fe2103980970f408522
senfox <vos_sen@mail.ru>
2021-03-01 12:22:53 +0300

    No time to cry
    Остатки ментального ресурса израсходованы.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(".user-menu").click(function() {
   $(this).toggleClass("show");
 });
 
-// Nf,kbxrf
-$('.data-expands').each(function(){
-	$(this).click(function(){
-		$(this).toggleClass('row-active');
-		$(this).parent().find('.expandable').toggleClass('row-open');
-	});
+// Expandable Data Table
+$(".js-clickable-row").each(function () {
+  $(this).click(function () {
+    $(this).toggleClass("row-active");
+    $(this).parent().find(".expandable").toggleClass("row-open");
+  });
 });
